aa5760833cea4c2a399c978603b74f79fb2b3989
seantan88 <65330251+seantan88@users.noreply.github.com>
2021-04-04 15:06:48 -0600

    Update screen.py

---
+++ screen.py
@@ -0,0 +1,15 @@
+class Screen:
+
+    def createMan(self):
+       head = ["O"]
+       neck = ["|"]
+       torsoArms = ["--|--"]
+       legs = ["/ \"]
+
+       body = [[][][][]]
+       for i in range(0), len(body):
+           print(body[i])
+
+       
+        
+        
